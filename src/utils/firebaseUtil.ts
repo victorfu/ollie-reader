d945654a0f746ba0c5838bf27d77e5df70bd1a0c
Victor Fu <supergothere@gmail.com>
2025-11-28 22:36:02 +0800

    refactor: move firebase configurations to env

--- src/utils/firebaseUtil.ts
+++ src/utils/firebaseUtil.ts
@@ -7,12 +7,12 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCTJXoPX-jETXzGDWiLBOsOeKbbxOwTLgw",
-  authDomain: "ollie-reader.firebaseapp.com",
-  projectId: "ollie-reader",
-  storageBucket: "ollie-reader.firebasestorage.app",
-  messagingSenderId: "661338251359",
-  appId: "1:661338251359:web:be5728533245203a18fbea",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 let firebaseApp: FirebaseApp;
 
