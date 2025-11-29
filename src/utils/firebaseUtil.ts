88a9a74d515c618f4e2540631cab469b1716f03f
Victor Fu <supergothere@gmail.com>
2025-11-29 23:48:51 +0800

    feat: Enables reCAPTCHA site key from env

--- src/utils/firebaseUtil.ts
+++ src/utils/firebaseUtil.ts
@@ -29,9 +29,7 @@ if (!getApps().length) {
   // Pass your reCAPTCHA v3 site key (public key) to activate(). Make sure this
   // key is the counterpart to the secret key you set in the Firebase console.
   initializeAppCheck(firebaseApp, {
-    provider: new ReCaptchaV3Provider(
-      "6LeIcfcrAAAAADXSRFIyoJFyVwlPbw-xRfgQgcQR",
-    ),
+    provider: new ReCaptchaV3Provider(import.meta.env.VITE_RECAPTCHA_SITE_KEY),
     isTokenAutoRefreshEnabled: true,
   });
 } else {
